d53b196bdc0cf2803606ad2271e35bef1e46d82b
IceZero <2224524274@qq.com>
2017-08-03 12:53:34 +0800

    the web by myself

---
+++ 响应式个人简历网站/script/main.js
@@ -0,0 +1,46 @@
+﻿$(function(){
+	var aboutme=$('a#aboutme');
+	var qiuzhi=$('a#qiuzhi');
+	var skill=$('a#skill');
+	var contact=$('a#contact');
+	aboutme.click(function(){
+	$('.aboutme').css('display','block');
+	$('.aboutme').animate({'marginBottom':'0'},1000);
+	})
+	$('.clock1').click(function(){
+	$('.aboutme').animate({'marginBottom':'50%'},1000);	
+	$('.aboutme').css('display','none');
+	})
+	$('.clock2').click(function(){
+	$('.aboutme').animate({'marginBottom':'50%'},1000);	
+	$('.aboutme').css('display','none');
+	})
+	qiuzhi.click(function(){
+	$('.qiuzhi').css('display','block');
+	$('.qiuzhi').animate({'marginLeft':'0'},1000);
+	})
+	$('.clock3').click(function(){
+	$('.qiuzhi').animate({'marginLeft':'100%'},1000);	
+	$('.qiuzhi').css('display','none');
+	})
+	skill.click(function(){
+	$('.skill').css('display','block');
+	$('.skill').animate({'marginTop':'0'},1000);
+	})
+	$('.clock4').click(function(){
+	$('.skill').animate({'marginTop':'50%'},1000);	
+	$('.skill').css('display','none');
+	})
+	$('.clock5').click(function(){
+	$('.skill').animate({'marginTop':'50%'},1000);	
+	$('.skill').css('display','none');
+	})
+	contact.click(function(){
+	$('.contact').css('display','block');
+	$('.contact').animate({'marginRight':'0'},1000);
+	})
+	$('.clock6').click(function(){
+	$('.contact').animate({'marginRight':'100%'},1000);	
+	$('.contact').css('display','none');
+	})
+})
